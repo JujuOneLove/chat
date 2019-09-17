f611261671ba869c91f7628637f277bf2ed8e080
Vanroy Julien <julienvanroy@yahoo.fr>
2019-09-17 23:26:54 +0200

    Début Projet
    Add UsersList,MessagesList,MessageBox

---
+++ src/components/MessagesList.vue
@@ -0,0 +1,16 @@
+<template>
+  <ul>
+    <li v-for="(message, index) in messages" :key="index">{{message}}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'MessagesList',
+  props: ['messages']
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
---
+++ src/views/Chat.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="chat">
+    <UsersList/>
+    <MessagesList :messages=messages />
+    <MessageBox @messageSent="onMessageSent"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import UsersList from '../components/UsersList.vue'
+import MessagesList from '../components/MessagesList.vue'
+import MessageBox from '../components/MessageBox.vue'
+
+export default {
+  name: 'chat',
+  data: function () {
+    return {
+      messages: []
+    }
+  },
+  components: {
+    UsersList,
+    MessagesList,
+    MessageBox
+  },
+  methods: {
+    onMessageSent (message) {
+      this.messages.push(message)
+    }
+  }
+}
+</script>
